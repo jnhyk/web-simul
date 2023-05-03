f6f14a37c19702f6d310ece7c5cfd8814e5073ab
jnhyk <hjh130101@gmail.com>
2023-05-03 15:07:46 +0900

    23.05.03

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const test = require("./Router/test");
+const main = require("./Router/main");
 const workbench = require("./Router/workbenchRoute");
 const assistant = require("./Router/assistantRoute");
 const energy = require("./Router/energyRoute");
 const pushdown = require("./Router/pushdownRoute");
 const ssh = require("./Router/sshRoute");
 
-app.use("/", test);
+app.use("/", main);
 app.use("/admin/workbench", workbench);
 app.use("/admin/assistant", assistant);
 app.use("/admin/energy", energy);
