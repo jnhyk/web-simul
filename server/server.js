daa6f57ce184645973b9f7c7ed9b7b604042567f
jnhyk <hjh130101@gmail.com>
2023-05-03 14:22:36 +0900

    23.05.03

--- server/server.js
+++ server/server.js
@@ -4,11 +4,15 @@ const test = require("./Router/test");
 const workbench = require("./Router/workbenchRoute");
 const assistant = require("./Router/assistantRoute");
 const energy = require("./Router/energyRoute");
+const pushdown = require("./Router/pushdownRoute");
+const ssh = require("./Router/sshRoute");
 
 app.use("/", test);
 app.use("/admin/workbench", workbench);
 app.use("/admin/assistant", assistant);
 app.use("/admin/energy", energy);
+app.use("/admin/pushdown", pushdown);
+app.use("/admin/ssh", ssh);
 
 const port = 5000; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
 app.listen(port, () => {
